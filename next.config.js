8356edbdecd97648dabf0f59ab6b218d590697c5
talimRockymountaintech <144800389+talimRockymountaintech@users.noreply.github.com>
2024-08-12 16:36:00 +0530

    add next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextCofig = {
+    output: 'standalone', // mine worked fine without this line
+    // ... other config
+  }
+  
+  /** @type {import('next').NextConfig} */
+  
+  // Remove this if you're not using Fullcalendar features
+  
+  module.exports = {
+    trailingSlash: true,
+    reactStrictMode: false,
+    experimental: {
+      esmExternals: false
+    },
+  
+    
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true
+    },
+    typescript: {
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      ignoreBuildErrors: true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "storage.googleapis.com",
+          port: "",
+        },
+      ],
+      domains: ["storage.googleapis.com"],
+    },
+  }
